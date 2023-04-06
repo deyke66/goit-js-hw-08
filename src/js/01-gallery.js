812a23f89cae4f7689273a0f5032a34f650447ec
Kurnosov Valentyn <deyke66@gmail.com>
2023-04-06 15:05:45 +0200

    make gallery again

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const gallerySpace = document.querySelector('.gallery');
 
+const markup = (function () {
+    const newMarkup = galleryItems.map(item =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt='${item.description}' title ='${item.description}' />
+   </a>
+</li>`)
+    return newMarkup
+})().join('');
+
+gallerySpace.innerHTML = markup
+
+const lightBox = new SimpleLightbox('.gallery a', {captionDelay:	250});
 // console.log(galleryItems);
