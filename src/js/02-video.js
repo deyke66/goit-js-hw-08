6e07f2bf740d8fcc7ea2cfe456ab0636e47cc61e
Kurnosov Valentyn <deyke66@gmail.com>
2023-04-09 18:11:49 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const TRACKERTIME_KEY = "videoplayer-current-time";
+
+const onPlay = function(data) {
+    let timeValue = JSON.stringify(data.seconds)
+
+    localStorage.setItem(TRACKERTIME_KEY, timeValue)
+
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.on('pause', throttle(onPlay, 1000));
+
+const currentTimeValue = JSON.parse(localStorage.getItem(TRACKERTIME_KEY)) || 0;
+
+player.setCurrentTime(currentTimeValue).then(function (seconds) {
+    seconds = currentTimeValue;
+});
